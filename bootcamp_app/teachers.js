b09ea258b764af3f09044fc8b0f47eac357031b8
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-01 21:18:55 -0700

    create teachers.js file

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+pool
+  .query(
+    `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+      FROM teachers
+      JOIN assistance_requests ON teachers.id = teacher_id
+      JOIN students ON students.id = student_id
+      JOIN cohortS ON cohorts.id = cohort_id
+      WHERE cohorts.name = '${process.argv[2]}' 
+      ORDER BY teachers.name;`
+  )
+  .then((res) => {
+    console.log("connected");
+    res.rows.forEach((data) => {
+      console.log(`${data.cohort} : ${data.teacher}`);
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
